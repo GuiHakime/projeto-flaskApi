5271f4d5f05581272b93ede13016e0d1ba7573ba
GuiHakime <guihakimeandrade@gmail.com>
2024-11-07 08:53:49 -0300

    Atualizações das confing do DB

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api, Resource
 import sqlalchemy
 import os
-
+from datetime import datetime
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 
@@ -15,9 +15,21 @@ api = Api(app)
 # Configuração do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'books.db')  # Usando SQLite, substitua conforme necessário
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desativa o rastreamento de modificações para evitar um aviso
+app.config['SQLALCHEMY_ECHO'] = True #Config para criar a primeira tabela
+
 
 #Criar instância do db
 db = SQLAlchemy(app)
+
+class Book_res(db.Model):
+    id=db.Column(db.Integer(), primary_key=True) #Primary_key sempre é o ID
+    title = db.Column(db.String(80), nullable = False)
+    autor = db.Column(db.String(40), nullable = False)
+    date_added = db.Column(db.DateTime(), default = datetime.utcnow)
+
+#Sempre preciso de um retorno então escolho o título
+    def __repr__(self):
+        return self.title
 
 @api.route('/books')   #ROTA apenas para postar
 class Books(Resource):
@@ -37,6 +49,15 @@ class BookResource(Resource):
     
     def delete(self, id):
         pass
+    
+
+@app.shell_context_processor
+def make_shell_context():
+    return{
+        'db':db,
+        'Book_res':Book_res
+    }
+    
     
 if __name__ == "__main__":
     app.run(debug=True)
